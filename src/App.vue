51569497fad5734d039c5de8e4ec15e92b6b74a3
Woomymy <github@woomy.ovh>
2020-11-09 12:08:16 +0100

    Added buttons to CSS

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <h1 class="center">TodoList</h1>
-  
+  <ul>
+    <li v-for="todo in todos" :key="todo.id" >{{ todo.id }}: {{ todo.value }}</li>
+  </ul>
+  <input id="adder">
+  <button id="validate" @click="addTodo()">Valider</button>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    addTodo() {
+      console.log("Test")
+    }
+  }
 };
 </script>
 <style>
@@ -17,5 +31,20 @@ export default {
   align-self: center;
   text-align: center;
   align-items: center;
+}
+input {
+  border-style: double;
+  border-color:  #288fef;
+}
+button {
+    margin: 10px;
+    background-color: #288fef;
+    border: none;
+    color: white;
+    padding: 13px 29px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 17px;
 }
 </style>
