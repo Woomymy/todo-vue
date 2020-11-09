8230bd4da368b1bb477a875282ab7c8097680222
Woomy <woomy@woomy.be>
2020-11-09 19:30:57 +0100

    Remove todos

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,17 +1,25 @@
 <template>
     <div class="todo" v-bind:id="todo.id"> 
         <li>{{ todo.id }}: {{ todo.value }}</li> <button class="tinybutton" @click="removeTodo(todo.id, todos)">Supprimer</button> 
-    
     </div>
 </template>
 <script>
 export default {
     name: 'TodoItem',
-    props: ['todo'],
+    props: ['todo', 'todos'],
     methods: {
         removeTodo(id) {
             let doc = document.getElementById(id);
             doc.parentNode.removeChild(doc);
+            let newtodos = []
+            this.todos.forEach(todo => {
+                if(todo.id == id) {
+                    return;
+                }
+                todo.id = newtodos.length + 1
+                newtodos.push(todo)
+            })
+            this.$emit('todoup', newtodos)
         }
     }
 
