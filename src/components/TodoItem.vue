5fcad748fbed0af13b4bfce088fb35b143e155e1
Woomymy <github@woomy.ovh>
2020-11-09 13:37:08 +0100

    Can now remove todo

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="todo" v-bind:id="todo.id"> 
+        <li>{{ todo.id }}: {{ todo.value }}</li> <button class="tinybutton" @click="removeTodo(todo.id)">Supprimer</button> 
+    </div>
+</template>
+<script>
+export default {
+    name: 'TodoItem',
+    props: ['todo'],
+    methods: {
+        removeTodo(id) {
+            let doc = document.getElementById(id);
+            doc.parentNode.removeChild(doc)
+        }
+    }
+
+}
+
+</script>
