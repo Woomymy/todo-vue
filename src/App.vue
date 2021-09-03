0b840be30179c134e85b30a96083edf43c00745d
Woomy <woomy@woomy.be>
2021-09-03 18:35:21 +0200

    Internals: Add addTodo() method

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
-  <h1>Hello, world</h1>
+  <h1 class="center">Todo</h1>
+  <input v-bind:id="TODO_CREATE_ID" @keypress.enter="addTodo()" />
 </template>
 
 <script setup lang="ts">
 import { defineComponent } from "vue";
+import { TODO_CREATE_ID, TODO_STORAGE_KEY } from "./constants";
+
+interface TodoItem {
+  content: string;
+}
+
+const saveTodos = (todos: TodoItem[]) => {
+  return window.localStorage.setItem(TODO_STORAGE_KEY, JSON.stringify(todos));
+};
+
+const getTodos = (): TodoItem[] => {
+  return JSON.parse(
+    window.localStorage.getItem(TODO_STORAGE_KEY) || "[]"
+  ) as TodoItem[];
+};
+
+const addTodo = () => {
+  const inputElement = document.getElementById(
+    TODO_CREATE_ID
+  ) as HTMLInputElement;
+  if (!inputElement || inputElement?.value === "") return;
+  const todos = getTodos();
+  todos.push({ content: inputElement.value });
+  saveTodos(todos);
+};
 
 defineComponent({
   name: "App",
@@ -11,4 +37,13 @@ defineComponent({
 });
 </script>
 
-<style></style>
+<style>
+* {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: #333;
+  color: white;
+}
+.center {
+  text-align: center;
+}
+</style>
