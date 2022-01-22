5fc78a93b8f60ae0ce5f56f8470b1e2ea1aec51b
Woomymy <github@woomy.ovh>
2022-01-22 15:36:59 +0100

    App: Implement todo deletion

--- src/components/TodoElement.vue
+++ src/components/TodoElement.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="todo" v-bind:content="todo.content">
-    <p>{{ todo.content }}</p>
+    <p>{{ todo.id }}: {{ todo.content }}</p>
+    <button @click="removeTodo(todo.id)">Delete</button>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+
 export default defineComponent({
   name: "TodoElement",
   props: ["todo"],
+  methods: {
+    removeTodo(id: number) {
+      this.$emit("todoDeletion", id);
+    },
+  },
 });
 </script>
