99823e9891712a7bf31be2423dc4eb6d329fd87b
Woomymy <github@woomy.ovh>
2022-01-22 14:44:33 +0100

    Main: Display todos on site

---
+++ src/components/TodoElement.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="todo" v-bind:content="todo.content">
+    <p>{{ todo.content }}</p>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "TodoElement",
+  props: ["todo"],
+});
+</script>
