a404fe2cb691be020d9db263d8057b2f7e05568f
Woomy <woomy@woomy.be>
2022-01-23 16:24:49 +0100

    App: Add basic todo editing

--- src/components/TodoElement.vue
+++ src/components/TodoElement.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="todo mb-2" v-bind:content="todo.content" :status="todo.status">
-    <p>
+    <p v-if="!editing" @click="editing = true">
       {{ todo.id }}: {{ todo.content }}<br />
       Status:
       <span class="todo-status">{{ todoStatusToString(todo.status) }}</span>
     </p>
+    <input :id="`edit-${todo.id}`" class="text-black" placeholder="Todo" v-if="editing" :value="todo.content" @keypress.enter="editTodo(todo.id)" />
     <button class="purple-button" type="button" @click="removeTodo(todo.id)">Delete</button>
    
     <button class="green-button" v-if="todo.status !== 'DONE'" @click="markAsDone(todo.id)">
@@ -29,6 +30,9 @@ export default defineComponent({
       required: true,
     },
   },
+  data() {
+    return { editing: false };
+  },
   methods: {
     /** Ask removeTodo() in parent to remove todo $id */
     removeTodo(id: number) {
@@ -41,6 +45,13 @@ export default defineComponent({
     /** Ask updateStatus() in parent to mark Todo as not started */
     markAsUnstarted(id: number) {
       this.$emit("statusUpdate", id, TodoStatus.NOT_STARTED);
+    },
+    /** Change todo content */
+    editTodo(id: number) {
+      const newInput = document.querySelector(`#edit-${id}`) as HTMLInputElement;
+      if (newInput.value == "") return;
+      this.$emit("todoEdition", id, newInput.value);
+      this.editing = false;
     },
     todoStatusToString,
   },
