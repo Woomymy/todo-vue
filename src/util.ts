5cf2c3df5fa708e9c23dcac2ddb9f10a87c3b2bb
Woomy <woomy@woomy.be>
2022-01-23 12:27:51 +0100

    Meta: Add more code comments

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,10 @@
 import { TodoStatus } from "./types";
 
+/**
+ * Transform TodoStatus into a string
+ * @param status New status of the todo
+ * @returns {string} Status in human-readable string
+ */
 export const todoStatusToString = (status: TodoStatus): string => {
   switch (status) {
     case TodoStatus.NOT_STARTED:
