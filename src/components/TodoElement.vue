10107067c1e2f9fa99e09e5d2acf84652c4ad549
Woomymy <github@woomy.ovh>
2022-01-23 11:30:20 +0100

    CSS: Restyle site with tailwindcss

--- src/components/TodoElement.vue
+++ src/components/TodoElement.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="todo" v-bind:content="todo.content" :status="todo.status">
+  <div class="todo mb-2" v-bind:content="todo.content" :status="todo.status">
     <p>
       {{ todo.id }}: {{ todo.content }}<br />
       Status:
       <span class="todo-status">{{ todoStatusToString(todo.status) }}</span>
     </p>
-    <button @click="removeTodo(todo.id)">Delete</button>
-    <button v-if="todo.status !== 'DONE'" @click="markAsDone(todo.id)">
+    <button class="purple-button" type="button" @click="removeTodo(todo.id)">Delete</button>
+   
+    <button class="green-button" v-if="todo.status !== 'DONE'" @click="markAsDone(todo.id)">
       Mark as done
     </button>
-    <button v-if="todo.status === 'DONE'" @click="markAsUnstarted(todo.id)">
+    <button class="red-button" v-if="todo.status === 'DONE'" @click="markAsUnstarted(todo.id)">
       Mark as not done
     </button>
   </div>
-  
 </template>
 <script lang="ts">
 import { TodoItem, TodoStatus } from "../types";
